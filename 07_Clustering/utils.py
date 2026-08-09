ef9fbbccd3e8140f66bd231ed126a57ee452d49c
Hoshang Warfade <166973278+Hoshang-Warfade@users.noreply.github.com>
2026-08-09 16:22:13 +0530

    Image Compression using K-means

--- 07_Clustering/utils.py
+++ 07_Clustering/utils.py
@@ -31,3 +31,148 @@ def plot_progress_kMeans(X, centroids, previous_centroids, idx, K, i):
         draw_line(centroids[j], previous_centroids[j])
     
     plt.title("Iteration number %d" %i)
+
+
+
+def find_closest_centroids(X, centroids):
+    """
+    Computes the centroid memberships for every example
+    
+    Args:
+        X (ndarray): (m, n) Input values      
+        centroids (ndarray): (K, n) centroids
+    
+    Returns:
+        idx (array_like): (m,) closest centroids
+    """
+    
+    # Set K
+    K = centroids.shape[0]
+
+    #to store assigment 
+    idx = np.zeros(X.shape[0], dtype=int)
+
+    for i in range(X.shape[0]):
+        dist_centroid = np.sum((centroids - X[i])**2, axis=1)
+        idx[i] = np.argmin(dist_centroid)
+
+    return idx
+
+    
+    
+    
+def compute_centroids(X, idx, K):
+    """
+    Returns the new centroids by computing the means of the 
+    data points assigned to each centroid.
+    
+    Args:
+        X (ndarray):   (m, n) Data points
+        idx (ndarray): (m,) Array containing index of closest centroid for each 
+                       example in X. Concretely, idx[i] contains the index of 
+                       the centroid closest to example i
+        K (int):       number of centroids
+    
+    Returns:
+        centroids (ndarray): (K, n) New centroids computed
+    """
+    
+    # Useful variables
+    m, n = X.shape
+    
+    
+    centroids = np.zeros((K, n))
+    
+    for i in range(K):
+        X_points =X[idx==i]
+        
+        if len(X_points) > 0:
+            centroids[i] = np.mean(X_points, axis=0)
+        
+        
+    
+    return centroids
+
+
+
+
+def run_kMeans(X, initial_centroids, max_iters=10, plot_progress=False):
+    """
+    Runs the K-Means algorithm on data matrix X, where each row of X
+    is a single example
+    """
+    
+    # Initialize values
+    m, n = X.shape
+    K = initial_centroids.shape[0]
+    centroids = initial_centroids
+    previous_centroids = centroids    
+    idx = np.zeros(m)
+    plt.figure(figsize=(8, 6))
+
+    # Run K-Means
+    for i in range(max_iters):
+        
+        #Output progress
+        print("K-Means iteration %d/%d" % (i, max_iters-1))
+        
+        # For each example in X, assign it to the closest centroid
+        idx = find_closest_centroids(X, centroids)
+        
+        # Optionally plot progress
+        if plot_progress:
+            plot_progress_kMeans(X, centroids, previous_centroids, idx, K, i)
+            previous_centroids = centroids
+            
+        # Given the memberships, compute new centroids
+        centroids = compute_centroids(X, idx, K)
+    idx = find_closest_centroids(X, centroids)
+    plt.show() 
+    return centroids, idx
+
+
+
+def kMeans_init_centroids(X, K):
+    """
+    This function initializes K centroids that are to be 
+    used in K-Means on the dataset X
+    
+    Args:
+        X (ndarray): Data points 
+        K (int):     number of centroids/clusters
+    
+    Returns:
+        centroids (ndarray): Initialized centroids
+    """
+    
+    # Randomly reorder the indices of examples
+    randidx = np.random.permutation(X.shape[0])
+    
+    # Take the first K examples as centroids
+    centroids = X[randidx[:K]]
+    
+    return centroids
+
+
+
+
+def plot_kMeans_RGB(X, centroids, idx, K):
+    # Plot the colors and centroids in a 3D space
+    fig = plt.figure(figsize=(16, 16))
+    ax = fig.add_subplot(221, projection='3d')
+    ax.scatter(*X.T*255, zdir='z', depthshade=False, s=.3, c=X)
+    ax.scatter(*centroids.T*255, zdir='z', depthshade=False, s=500, c='red', marker='x', lw=3)
+    ax.set_xlabel('R value - Redness')
+    ax.set_ylabel('G value - Greenness')
+    ax.set_zlabel('B value - Blueness')
+    ax.set_title("Original colors and their color clusters' centroids")
+    plt.show()
+
+
+def show_centroid_colors(centroids):
+    palette = np.expand_dims(centroids, axis=0)
+    num = np.arange(0,len(centroids))
+    plt.figure(figsize=(16, 16))
+    plt.xticks(num)
+    plt.yticks([])
+    plt.imshow(palette)
